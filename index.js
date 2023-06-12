f8079cb882731f7436f4806f93b14941757fcc42
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-06-12 15:34:31 +0300

    arithmatic lab DONE

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-
 // Write your code here
+let num1 = 31,
+  num2 = 2;
+let multiply = num1 * num2;
+
+/**------------------------- */
+let random = Math.floor(Math.random() * 10);
+/**------------------------- */
+let num3 = 14,
+  num4 = 10;
+let mod = num3 % num4;
+/**------------------------- */
+let max = Math.max(7, 15, 18, 3, 6, 13, 20)
+ 
+// let numberArr = [7, 15, 18, 3, 6, 13, 20];
+// function highest (numberArr){
+//     let max =0;
+//     for (const iterator of numerArr) {
+        
+//         if (iterator > max){
+//             max = iterator
+            
+//         }
+//         return max
+//     }
+
+// }
